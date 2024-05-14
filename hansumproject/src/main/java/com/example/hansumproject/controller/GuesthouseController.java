09281554143812ab2775875afd58701f46e00132
김동욱 <bradl0809@naver.com>
2024-05-14 10:58:57 +0900

    feat: 게스트하우스 이미지 조회 api 구현

--- hansumproject/src/main/java/com/example/hansumproject/controller/GuesthouseController.java
+++ hansumproject/src/main/java/com/example/hansumproject/controller/GuesthouseController.java
@@ -1,26 +1,52 @@
 package com.example.hansumproject.controller;
 
+import com.example.hansumproject.dto.ReservationDto;
+import com.example.hansumproject.entity.ReservationEntity;
 import com.example.hansumproject.repository.GuesthouseRepository;
 import com.example.hansumproject.service.GuesthouseService;
+import com.example.hansumproject.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/guesthouses")
 public class GuesthouseController {
 
     @Autowired
     private GuesthouseService guesthouseService;
 
+    @Autowired
+    private ImageService imageService;
+
     // 게스트하우스 상세 정보 조회
-    @GetMapping("/guesthouses/{guesthouseId}")
+    @GetMapping("/{guesthouseId}")
     public ResponseEntity<?> getGuesthouseDetail(@PathVariable Long guesthouseId) {
         return guesthouseService.getGuesthouseDetail(guesthouseId);
     }
 
+    // 게스트하우스 이미지 조회
+    @GetMapping("/{guesthouseId}/{imageUrl}")
+    public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable("imageUrl") String fileName) throws IOException {
+        byte[] downloadImage = imageService.downloadImageFromFileSystem(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/jpeg"))
+                .body(downloadImage);
+    }
 
+    // 추천 게스트하우스 조회
+
+
+    // 게스트하우스 예약
+    @PostMapping("/{guesthouseId}")
+    public ResponseEntity<?> createReservation(@PathVariable Long guesthouseId, @RequestBody ReservationDto reservationDto) {
+        return guesthouseService.createReservation(guesthouseId, reservationDto);
+    }
 }
